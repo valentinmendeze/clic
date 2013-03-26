f2a17bbc547681a1ca15286af9426feaaebd6c7c
Milan <milan.ricoul@gmail.com>
2013-03-27 00:43:28 +0100

    delete page-gallery

--- wp-content/themes/csf/single.php
+++ wp-content/themes/csf/single.php
@@ -1,13 +1,4 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
- */
-
-get_header(); ?>
+<?php get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
