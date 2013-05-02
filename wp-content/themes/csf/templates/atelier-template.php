ea2c79d937cd0ff0ef8794b829e47716b90850c4
Milan <milan.ricoul@gmail.com>
2013-05-02 22:48:10 +0200

    Intégration Ateliers terminé
    (ou presque ?)

--- wp-content/themes/csf/templates/atelier-template.php
+++ wp-content/themes/csf/templates/atelier-template.php
@@ -24,7 +24,7 @@ Template Name: Ateliers
 	$atelier_fini = new WP_query(array(
 		'post_type' => 'atelier',
 		'statut' => 'atelier-termine',
-		'posts_per_page' => 10
+		'posts_per_page' => 8
 	));
 
 ?>
@@ -32,6 +32,7 @@ Template Name: Ateliers
 	<section id="subhead" class="subhead subhead-atelierarchive">
 		<hgroup>
 			<h1 class="site-title">Les ateliers</h1>
+			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 	</section>
 
@@ -41,23 +42,28 @@ Template Name: Ateliers
 				<?php if($atelier->have_posts()): while($atelier->have_posts()): $atelier->the_post(); ?>
 				<li class="span4">
 					<div class="thumbnail">
-						<?php the_post_thumbnail('cover'); ?>
-						<h3><?php the_title(); ?></h3>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('cover'); ?></a>
+						<h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
 						<?php the_excerpt(); ?>
+						<?php $dated = DateTime::createFromFormat('Ymd', get_field('date_de_commencement')); ?>
+						<?php $datef = DateTime::createFromFormat('Ymd', get_field('date_de_fin')); ?>
 						<ul class="infos">
-							<li>Date de début :</li>
-							<li>Date de fin :</li>
-							<li>Lieu :</li>
+							<li class="dated">Date de début : <span class="bold"><?php echo $dated->format('d/m/Y'); ?></span></li>
+							<li class="datef">Date de fin : <span class="bold"><?php echo $datef->format('d/m/Y'); ?></span></li>
+							<li class="lieu">Lieu : <span class="bold"><?php the_field('lieu_de_latelier') ?></span></li>
 						</ul>
 						<a href="<?php the_permalink(); ?>" title="Découvrir l'atelier" class="more"><span>Découvrir l'atelier</span></a>
 					</div>
 				</li>
 				<?php endwhile; endif; ?>
-				<li class="span4">
+				<li class="span4 ateliers-finis">
 					<div class="thumbnail">
-						<img data-src="holder.js/300x200" alt="">
-						<h3>Thumbnail label</h3>
-						<p>Thumbnail caption...</p>
+						<h3 class="last-thumb-title">Les ateliers terminés</h3>
+						<div class="picture">
+							<?php if($atelier_fini->have_posts()): while($atelier_fini->have_posts()): $atelier_fini->the_post(); ?>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('cover'); ?></a>
+							<?php endwhile; endif; ?>
+						</div>
 					</div>
 				</li>
 			</ul>
@@ -67,35 +73,3 @@ Template Name: Ateliers
 	<?php wp_reset_query(); ?>
 
 <?php get_footer(); ?>
-
-	<!--<?php if($atelier_phare->have_posts()): $atelier_phare->the_post(); ?>
-
-	<section class="last-project container-fluid">
-		<div class="row-fluid">
-			<div class="span12">
-				<div class="thumb span8">
-				<?php if(has_post_thumbnail()): ?>
-					<?php the_post_thumbnail('cover'); ?>
-				<?php else: ?>
-					<img src="<?php bloginfo('template_url'); ?>/images/example.png" title="<?php the_title(); ?>">
-				<?php endif; ?>
-					<div class="clear"></div>
-				</div>
-				<article class="span4">
-					<h2><?php the_title(); ?></h2>
-					<?php if (get_field('quick_description')): ?>
-					<p><?php the_field('quick_description'); ?></p>
-					<?php else: ?>
-					<p><?php the_excerpt(); ?></p>
-					<?php endif; ?>
-					<div class="commands">
-						<a href="<?php the_field('donate-link'); ?>" title="Faire un don"><div class="buttons donate">Faire un don</div></a>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php bloginfo('template_url') ?>/images/more.png" class="more" alt="En savoir plus"><div class="discover">Découvrir le projet</div></a>
-						<div class="clear"></div>
-					</div>
-				</article>
-			</div>
-		</div>
-	</section>
-
-	<?php endif; ?>-->
