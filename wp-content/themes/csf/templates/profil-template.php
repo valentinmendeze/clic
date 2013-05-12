5311c1eb8542c0182c2dfbae41dce1bcb6ae8d41
Milan <milan.ricoul@gmail.com>
2013-05-12 22:22:45 +0200

    Page profil

--- wp-content/themes/csf/templates/profil-template.php
+++ wp-content/themes/csf/templates/profil-template.php
@@ -1,3 +1,15 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Profil
+*/
+
+$user = wp_get_current_user();
+
+if($user->ID == 0) {
+	wp_redirect( 'login');
+	exit();
+}
+
+get_header(); ?>
 
 <?php get_footer(); ?>
